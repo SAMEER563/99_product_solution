28249ef86051686c3d301cba557f0ddc5a386fd3
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-10-18 11:35:06 +0530

    About page change

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -11,15 +11,15 @@ const AboutUs = () => {
         <h1 className="text-3xl md:text-6xl font-semibold text-black text-start ml-4 md:ml-16 mt-4">
           Who we are
         </h1>
-        <p className="text-start text-black text-base md:text-[18px] font-[400] mt-4 ml-4 md:ml-16">
-          99Products Solutions is a dynamic IT service provider, specializing in
+        <p className="text-start text-black text-base md:text-[16px] font-lg mt-4 ml-4 md:ml-16">
+          99Product Solutions is a dynamic IT service provider, specializing in
           building tailored websites and mobile applications to meet the unique
           needs of businesses. We offer a wide range of IT services, including
           cloud solutions, DevOps, and IT consulting, with a focus on delivering
           scalable, innovative, and cost-effective solutions.
           <br />
           <br />
-          At 99products Solutions, we pride ourselves on our customer-centric
+          At 99product Solutions, we pride ourselves on our customer-centric
           approach, ensuring that our clients receive the highest level of
           service, from consultation to implementation. We partner with
           organizations to transform their digital presence and optimize their
@@ -37,7 +37,7 @@ const AboutUs = () => {
         {/* Background circular patterns */}
         <div className="relative flex justify-center items-end w-full h-full ">
           {/* Circular pattern */}
-          <div className="absolute left-[550px] bottom-[150px] lg:left-[520px] md:left-[420px] md:bottom-[300px] lg:bottom-[180px] transform rotate-180 ">
+          <div className="absolute left-[550px] bottom-[150px] lg:left-[520px] md:left-[420px] md:bottom-[300px] lg:bottom-[120px] transform rotate-180 ">
             <div className="w-[300px] h-[300px] rounded-full border-[1px] border-purple-800 opacity-50 absolute top-11 left-11"></div>
             <div className="w-[320px] h-[320px] rounded-full border-[1px] border-purple-800 opacity-50 absolute top-10 left-10"></div>
             <div className="w-[340px] h-[340px] rounded-full border-[1px] border-purple-800 opacity-50 absolute top-9 left-9"></div>
@@ -54,7 +54,7 @@ const AboutUs = () => {
 
         {/* Floating square panels */}
         <div className="flex space-x-4 md:space-x-8">
-          <div className="absolute top-28 md:top-[420px] left-4 md:left-6 w-[200px] lg:top-[200px] lg:left-[120px] h-[200px] md:w-[230px] md:h-[230px] bg-white bg-opacity-30 backdrop-blur-lg rounded-xl"></div>
+          <div className="absolute top-28 md:top-[420px] left-4 md:left-6 w-[200px] lg:top-[160px] lg:left-[60px] h-[200px] md:w-[230px] md:h-[230px] bg-white bg-opacity-30 backdrop-blur-lg rounded-xl"></div>
           <div className="w-[200px] h-[200px] md:w-[230px] md:h-[230px] absolute top-48 md:top-[520px] left-4 md:left-16 lg:top-[300px] lg:left-[180px] bg-white bg-opacity-30 backdrop-blur-lg rounded-xl"></div>
         </div>
       </div>
