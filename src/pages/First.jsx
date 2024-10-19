c1544c1c1b96bc406b92ed65f8eb6de0028fe626
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-10-19 16:38:52 +0530

    change

--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -46,7 +46,7 @@ const First = () => {
           <img
             src="/first_bg.png"
             alt="Background Graphic"
-            className="w-[300px] md:w-[600px] h-auto mt-[100px] mr-100px lg:mt-[490px] lg:w-[1320px] lg:ml-[-100px] lg:h-[824px] "
+            className="w-[300px] md:w-[600px] h-auto mt-[100px] mr-100px lg:mt-[460px] lg:w-[1320px] lg:ml-[-100px] lg:h-[824px] "
           />
           </div>
           {/* Floating Panels */}
